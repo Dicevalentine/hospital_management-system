c05fae93f5f9bf25a2daac5e2f9f0ad622a930d5
Dicevalentine <Loluv50@gmail.com>
2019-04-17 10:58:56 +0300

    project

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn=mysqli_connect("localhost","root","","hospital_records")or die(mysqli_error($conn));
-$id=$_GET['id'];
-$delete_query=mysqli_query($conn,"delete from patients_records where id=$id")or die(mysqli_error($conn));
-if($delete_query){
-    echo "<script>window.alert('record has been deleted')</script>";
-    header("refresh:0;url=dashboard.php");
-}
-?>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('process_login.php');
-session_destroy();
-echo "Logging out.................";
-header('refresh:2;url=index.php');
-
-?>
